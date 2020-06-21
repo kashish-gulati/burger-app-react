377b3cc7eaa3bf62e4ba7678c9fedb1fb28d10ff
Kashish Gulati <kashlati@gmail.com>
2020-06-22 00:30:46 +0530

    added sidedrawer for mobile phones

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import styles from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
+
+const SideDrawer = (props) => {
+    let attachedStyles = [styles.SideDrawer, styles.Close];
+    if (props.open) {
+        attachedStyles = [styles.SideDrawer,styles.Open];
+    }
+    
+    return (
+        <Aux>
+            <Backdrop show = {props.open} clicked = {props.closed} />
+            <div className = {attachedStyles.join(' ')} >
+                <div className = {styles.Logo} >
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Aux>
+    );
+};
+
+export default SideDrawer;
